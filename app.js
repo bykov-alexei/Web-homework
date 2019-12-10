16ea3ab6fba1bbf9a8668a65c78cc9f473ee211c
Alexei <bitskoff@gmail.com>
2019-12-10 16:32:11 +0800

    raw-script

---
+++ app.js
@@ -0,0 +1,80 @@
+function find_input(form, name) {
+    return form.querySelectorAll(`.field input[name^='${name}']`);
+}
+
+function apply(form_id) {
+    let form = document.getElementById(form_id);
+
+    let company = find_input(form, 'company-name')[0].value;
+
+    let person_input = find_input(form, 'person-name')[0];
+    let person = person_input.value;
+    let person_align =  Array.prototype.slice.call(person_input.parentNode.querySelector(".align .selected").classList).
+         filter(s => (s.substring(0, "align".length) === "align"))[0].split('-')[1];
+    let person_size = person_input.parentNode.querySelector(".size .selected").innerText;
+    let person_color = window.getComputedStyle(person_input.parentNode.querySelector(".color-switch .color-selected")).borderRightColor;
+    alert(person_color.currentStyle);
+    let position_input = find_input(form, 'position')[0];
+    let position = position_input.value;
+    let position_align = Array.prototype.slice.call(position_input.parentNode.querySelector(".align .selected").classList).
+    filter(s => (s.substring(0, "align".length) === "align"))[0].split('-')[1];
+    let position_size = position_input.parentNode.querySelector(".size .selected").innerText;
+
+    let phones = Array.prototype.slice.call(find_input(form, 'phone-number')).map(input => input.value);
+    let email = find_input(form, 'email')[0].value;
+    let address = find_input(form, 'address')[0].value;
+
+    let company_display = form.querySelector(".display p.company-name");
+    let name_display = form.querySelector(".display p.person-name");
+    let position_display = form.querySelector(".display p.position");
+    let phones_display = form.querySelectorAll(".display .additional")[0];
+    let email_display = form.querySelectorAll(".display .additional")[1];
+    let address_display = form.querySelectorAll(".display .additional")[2];
+
+    company_display.innerText = company;
+
+    name_display.innerText = person;
+    name_display.style.textAlign = person_align;
+    name_display.style.fontSize = person_size;
+
+    position_display.innerText = position;
+    position_display.style.textAlign = position_align;
+    position_display.style.fontSize = position_size;
+
+    phones_display.innerHTML = "";
+    for (let phone of phones) {
+        let elem = document.createElement("p");
+        elem.innerText = phone;
+        phones_display.appendChild(elem);
+    }
+
+    email_display.innerText = email;
+    address_display.innerText = address;
+}
+
+function onColorChange(event) {
+    let colors_container = event.target.parentNode;
+    for (let children of colors_container.children) {
+        children.classList.remove("color-selected");
+    }
+    event.target.classList.add("color-selected");
+}
+
+function onOptionChange(event) {
+    let switch_container = event.target.parentNode;
+    for (let option of switch_container.children) {
+        option.classList.remove("selected");
+    }
+    event.target.classList.add('selected');
+}
+
+
+let color_options = document.querySelectorAll(".color-option");
+for (let option of color_options) {
+    option.addEventListener("click", onColorChange);
+}
+
+let options = document.querySelectorAll(".option");
+for (let option of options) {
+    option.addEventListener('click', onOptionChange);
+}
