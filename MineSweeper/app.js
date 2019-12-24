a9e44e4cd5b0b01f047806d040736fc5ddfc2cbb
Alexei-LAPTOP <bitskoff@gmail.com>
2019-12-24 16:03:15 +0800

    restarted-mine-sweeper

--- MineSweeper/app.js
+++ MineSweeper/app.js
@@ -1,231 +1,132 @@
 function drawField(event) {
-    let m_input = event.target.previousElementSibling;
-    let n_input = m_input.previousElementSibling;
-    let n = n_input.value;
+    let settings = event.target.parentNode;
+    let field = settings.parentNode.parentNode.querySelector('.field');
 
-    let fieldDiv = n_input.parentElement.previousElementSibling.querySelector(".field");
-    fieldDiv.innerHTML = "";
+    let n, m;
+    n = +settings.children[0].querySelector('input').value;
+    m = +settings.children[1].querySelector('input').value;
 
+    field.setAttribute('n', n);
+    field.setAttribute('m', m);
+
+    field.innerHTML = "";
     for (let i = 0; i < n; i++) {
-        let fieldRow = document.createElement("div");
-        fieldRow.classList.add("row");
-        fieldDiv.appendChild(fieldRow);
+        let row = document.createElement('div');
+        row.classList.add('row');
+        field.appendChild(row);
+
         for (let j = 0; j < n; j++) {
-            let cell = document.createElement("div");
-            cell.classList.add("cell");
-            cell.animationDelay = String(i);
-            cell.animationDuration = String(j);
-			cell.removeEventListener('click', drawField);
-            cell.addEventListener('click', cellClicked);
-            cell.addEventListener('contextmenu', cellFlagged);
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.setAttribute('x', j);
+            cell.setAttribute('y', i);
+            cell.addEventListener('click', cellClick);
             cell.addEventListener('mine.step', step);
-            fieldRow.appendChild(cell);
+            row.appendChild(cell);
         }
     }
 }
 
-function cellClicked(event) {
-    let cell = event.target;
-    let row = cell.parentNode;
-    let field = row.parentNode;
-    let x, y;
-    for (x = 0; row.children[x] !== cell; x++);
-    for (y = 0; field.children[y] !== row; y++);
-    let opened_cells = field.querySelectorAll(".opened");
-    if (opened_cells.length === 0) {
-        let cEvent = new CustomEvent("mine.start", {detail: {x: x, y: y}});
-        field.dispatchEvent(cEvent);
-    }
-    cell.dispatchEvent(new CustomEvent("mine.step", {detail: {x: x, y: y, click: true}}));
-}
 
-function cellFlagged(event) {
-    let cell = event.target;
-    cell.classList.toggle('flagged');
-    if (cell.classList.contains('flagged')) {
-        cell.removeEventListener('click', cellClicked);
-    } else {
-        cell.addEventListener('click', cellClicked);
-    }
-    return false;
-}
-
-function getCell(field, i, j) {
-    return field.querySelectorAll(".row")[j].querySelectorAll(".cell")[i];
-}
-
-function openCell(cell) {
-    cell.classList.add('opened');
-    cell.removeEventListener('mine.step', step);
-    cell.removeEventListener('contextmenu', cellFlagged);
-}
-
-function start(event) {
-    let n, m;
-    let x, y;
+function generateField(event) {
     let field = event.target;
-    let settings = field.parentNode.parentNode.querySelector('.settings');
-    n = settings.children[0].value;
-    m = +settings.children[1].value;
-    x = event.detail.x;
-    y = event.detail.y;
+    let x = +event.detail.x;
+    let y = +event.detail.y;
+    let n = +field.getAttribute('n');
+    let m = +field.getAttribute('m');
 
     let bombs = 0;
     while (bombs !== m) {
-        let i = Math.round(Math.random() * (n - 1));
-        let j = Math.round(Math.random() * (n - 1));
-        let cell = getCell(field, i, j);
-        if ((i !== y) && (j !== x) && (!cell.classList.contains('bombed'))) {
-            cell.classList.add('bombed');
-            bombs += 1;
+        let i, j;
+        i = Math.round(Math.random() * (n - 1));
+        j = Math.round(Math.random() * (n - 1));
+        let row = field.children[j];
+        let cell = row.children[i];
+        if ((!(i === x && j === y)) && (!cell.classList.contains('bombed'))) {
+            cell.classList.add("bombed");
+            cell.classList.add("opened");
+            bombs++;
         }
     }
 
     for (let i = 0; i < n; i++) {
+        let row = field.children[i];
         for (let j = 0; j < n; j++) {
-            let cell = getCell(field, i, j);
-            if (cell.classList.contains('bombed'))
-                continue;
-            let counter = 0;
-            for (let dx = -1; dx < 2; dx++) {
-                for (let dy = -1; dy < 2; dy++) {
-                    if (i + dx >= 0 && i + dx < n && j + dy >= 0 && j + dy < n) {
-                        let c_cell = getCell(field, i + dx, j + dy);
-                        if (c_cell.classList.contains('bombed')) {
-                            counter++;
+            let cell = row.children[j];
+
+            if (!cell.classList.contains('bombed')) {
+                let v = 0;
+                for (let dx = -1; dx <= 1; dx++) {
+                    for (let dy = -1; dy <= 1; dy++) {
+                        if (i + dx >= 0 && i + dx < n && j + dy >= 0 && j + dy < n) {
+                            if (getCell(field, i + dx, j + dy).classList.contains('bombed')) {
+                                v++;
+                            }
                         }
                     }
                 }
+                if (v !== 0) {
+                    cell.innerText = String(v);
+                    cell.classList.add("c" + v);
+                }
             }
-            cell.classList.add('c' + counter);
-            if (counter)
-                cell.innerHTML = counter;
+        }
+    }
+}
+
+function getCell(field, i, j) {
+    let row = field.children[i];
+    return row.children[j];
+}
+
+function openCell(cell) {
+    let x = +cell.getAttribute('x');
+    let y = +cell.getAttribute('y');
+    console.log(x + " " + y);
+    cell.classList.add("opened");
+    if (cell.innerText === "") {
+        for (let dx = -1; dx <= 1; dx++) {
+            for (let dy = -1; dy <= 1; dy++) {
+                if (!cell.classList.contains('bombed') && !cell.classList.contains('opened')) {
+                    openCell(getCell(field, x + dx, y + dy));
+                }
+            }
         }
     }
 }
 
 function step(event) {
     let cell = event.target;
-    let x = event.detail.x;
-    let y = event.detail.y;
-    let n = event.target.parentNode.children.length;
-    let field = cell.parentNode.parentNode;
-    if (cell.innerText === "" && !cell.classList.contains('bombed')) {
-        openCell(cell);
-        if (x - 1 >= 0) {
-            let tmp_cell = getCell(field, x - 1, y);
-            if (!tmp_cell.classList.contains('opened'))
-                tmp_cell.dispatchEvent(new CustomEvent('mine.step', {detail: {x: x - 1, y: y}}));
-        }
-        if (x + 1 < n) {
-            let tmp_cell = getCell(field, x + 1, y);
-            if (!tmp_cell.classList.contains('opened'))
-                tmp_cell.dispatchEvent(new CustomEvent('mine.step', {detail: {x: x + 1, y: y}}));
-        }
-        if (y - 1 >= 0) {
-            let tmp_cell = getCell(field, x, y - 1);
-            if (!tmp_cell.classList.contains('opened'))
-                tmp_cell.dispatchEvent(new CustomEvent('mine.step', {detail: {x: x, y: y - 1}}));
-        }
-        if (y + 1 < n) {
-            let tmp_cell = getCell(field, x, y + 1);
-            if (!tmp_cell.classList.contains('opened'))
-                tmp_cell.dispatchEvent(new CustomEvent('mine.step', {detail: {x: x, y: y + 1}}));
-        }
-    } else if (cell.innerText !== "") {
-        openCell(cell);
-    } else if (event.detail.click) {
-        openCell(cell);
-        if (cell.classList.contains('bombed')) {
-            field.dispatchEvent(new CustomEvent('mine.end'));
-        }
-    }
-    let closed_cells = field.querySelectorAll('.cell:not(.opened), .cell.flagged');
-    let bombed_cells = field.querySelectorAll('.cell.bombed');
-    if (closed_cells.length === bombed_cells.length) {
-        field.dispatchEvent(new CustomEvent('mine.end'));
-    }
-}
-
-function end(event) {
-    let field = event.target;
-    let cells = field.querySelectorAll('.cell');
-    for (let cell of cells) {
-        cell.classList.add('opened');
-    }
-}
-
-function keyboardHandler(event) {
-    console.log(event.key);
-    let field = document.querySelector('.field');
-    let cell = field.querySelector('.selected');
-
-    if (!cell) {
-        let cell = field.querySelectorAll('.cell')[0];
-        cell.classList.add('selected');
-        return;
-    }
-
-
-    let row = cell.parentNode;
     let x, y;
-    for (x = 0; row.children[x] !== cell; x++);
-    for (y = 0; field.children[y] !== row; y++);
-    if (event.key === 'ArrowUp') {
-        let upper_row = row.previousSibling;
-        if (!upper_row) {
-            upper_row = row.parentNode.lastChild;
-        }
-        upper_row.children[x].classList.add('selected');
-        cell.classList.remove('selected');
+    // x = cell.getAttribute('x');
+    // y = cell.getAttribute('y');
+    openCell(cell);
 
+}
 
-    } else if (event.key === 'ArrowDown') {
-        let lower_row = row.nextSibling;
-        if (!lower_row) {
-            lower_row = row.parentNode.firstChild;
-        }
-        lower_row.children[x].classList.add('selected');
-        cell.classList.remove('selected');
-    } else if (event.key === 'ArrowRight') {
-        let next_cell = cell.nextSibling;
-        if (!next_cell) {
-            next_cell = row.firstChild;
-        }
-        cell.classList.remove('selected');
-        next_cell.classList.add('selected');
-    } else if (event.key === 'ArrowLeft') {
-        let prev_cell = cell.previousSibling;
-        if (!prev_cell) {
-            prev_cell = row.lastChild;
-        }
-        cell.classList.remove('selected');
-        prev_cell.classList.add('selected');
-    } else if (event.key === ' ' || event.key === 'Enter') {
-        if (event.ctrlKey === true) {
-            cellFlagged({target: cell});
-        } else {
-            cellClicked({target: cell});
-        }
+function cellClick(event) {
+    let cell = event.target;
+    let field = cell.parentNode.parentNode;
+    let x = cell.getAttribute('x');
+    let y = cell.getAttribute('y');
+    if (field.querySelectorAll('.bombed').length === 0) {
+        field.dispatchEvent(new CustomEvent("mine.start", {detail:{x:x, y:y}}));
     }
 
+    let mEvent = new CustomEvent('mine.step');
+    mEvent.isTrusted = true;
+    cell.dispatchEvent(mEvent);
+
+    openCell(cell);
+    if (cell.classList.contains('bombed')) {
+        field.dispatchEvent(new CustomEvent('mine.end', {detail: {result: "lose"}}));
+    }
 }
 
-
-
-let startButtons = document.querySelectorAll('.start-button');
-for (let button of startButtons) {
-    button.addEventListener('click', drawField);
+let start_buttons = document.querySelectorAll('button');
+for (let start_button of start_buttons) {
+    start_button.addEventListener('click', drawField);
 }
 
-let fields = document.querySelectorAll(".field");
-for (let field of fields) {
-    field.addEventListener('mine.start', start);
-    field.addEventListener('mine.end', end);
-}
-
-window.oncontextmenu = function () {
-    return false;
-};
-document.addEventListener('keyup', keyboardHandler);
-
+let field = document.querySelector('.field');
+field.addEventListener('mine.start', generateField);
